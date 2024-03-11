a338c6ca916cdff13e0a01536df36f058c01a731
Aubrey Whitford <Aubreya.whitford@gmail.com>
2024-03-11 13:46:03 -0400

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your solution in this file!
+let employee = {
+    name: 'Aubrey',
+    streetAddress: '742b Pinebrook rd',
+};
+
+function updateEmployeeWithKeyAndValue(original, key, value) {
+    return {
+        ...original,
+        [key]: value,
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee; 
+}
+
+function deleteFromEmployeeByKey(original, key) {
+    const clonedEmployee = { ...original };
+    delete clonedEmployee[key];
+    return clonedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+
+
+let updatedEmployee = updateEmployeeWithKeyAndValue(employee, 'name', 'Sam');
+updatedEmployee = updateEmployeeWithKeyAndValue(updatedEmployee, 'streetAddress', '11 Broadway');
+console.log(updatedEmployee);
+
+destructivelyUpdateEmployeeWithKeyAndValue(employee, 'name', 'Sam');
+destructivelyUpdateEmployeeWithKeyAndValue(employee, 'streetAddress', '11 Broadway');
+
+
+let employeeWithoutName = deleteFromEmployeeByKey(employee, 'name');
+console.log(employeeWithoutName);
+
